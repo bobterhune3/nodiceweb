feff4ee7964e737c2a14f9ee6cf04c1e82ec1462
bobterhune3 <bobterhune3@gmail.com>
2015-12-07 07:01:31 -0800

    Initial Version

---
+++ nodiceweb.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nodiceweb;
+using nodiceweb.Controllers;
+using nodiceweb.Models;
+
+namespace nodiceweb.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexOrdersByName()
+        {
+            var context = new FakeTeamContext
+            {
+                Teams =
+                    {
+                        new Team { Name = "BBB"},
+                        new Team { Name = "AAA"},
+                        new Team { Name = "ZZZ"},
+                    }
+            };
+
+            var controller = new TeamsController();
+            var result = controller.Index();
+
+            Assert.IsInstanceOfType(result.ViewData.Model, typeof(IEnumerable<Team>));
+
+            var departments = (IEnumerable<Team>)result.ViewData.Model;
+            Assert.AreEqual("AAA", departments.ElementAt(0).Name);
+            Assert.AreEqual("BBB", departments.ElementAt(1).Name);
+            Assert.AreEqual("ZZZ", departments.ElementAt(2).Name);
+        }
+    }
+}
---
+++ nodiceweb/Models/FakeTeamContext.cs
@@ -0,0 +1,25 @@
+﻿using nodiceweb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace nodiceweb.Models
+{
+    public class FakeTeamContext : INoDiceContext
+    {
+        public FakeTeamContext()
+        {
+            this.Teams = new FakeTeamSet();
+        }
+
+        public IDbSet<Team> Teams { get; private set; }
+
+        public int SaveChanges()
+        {
+            return 0;
+        }
+    }
+}
